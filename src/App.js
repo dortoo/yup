fe5c72abd2b8931c5d6389516459163d4397f7f8
dortoo <dorto@live.com>
2022-05-27 18:48:37 -0300

    getting data & validating

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { userSchema } from "./Validations/UserValidation";
 
 function App() {
+
+  const createUser = async (event) => {
+    event.preventDefault()
+    let formData = {
+      name: event.target[0].value,
+      email: event.target[1].value,
+      password: event.target[2].value,
+    };
+    const isValid = await userSchema.isValid(formData);
+    console.log(isValid);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={createUser}>
+        <input type="text" placeholder="Name..." />
+        <input type="text" placeholder="email@email.com" />
+        <input type="text" placeholder="password123" />
+        <input type="submit"/>
+      </form>
     </div>
-  );
+  )
 }
 
 export default App;
